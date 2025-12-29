e326ba06fa4f704ee28a54641fb55d622ce2943d
Fernando <ferbrizie@gmail.com>
2025-12-29 16:11:36 +0100

    ex00 complete. Check for more tests when back to spain

---
+++ ex00/Bureaucrat.hpp
@@ -0,0 +1,32 @@
+#include "string"
+#include <iostream>
+#include <exception>
+
+class Bureaucrat
+{
+	private:
+		 const	std::string name;
+		 int	grade;
+	public:
+		 Bureaucrat();
+		 Bureaucrat(const std::string name, int grade);
+		 Bureaucrat(const Bureaucrat &other);
+		 Bureaucrat&	operator=(const Bureaucrat &other);
+		 ~Bureaucrat();
+		 class GradeTooHighException: public std::exception
+		 {
+			 public:
+				 const char* what() const throw();
+		 };
+		 class GradeTooLowException: public std::exception
+		 {
+			 public:
+				 const char* what() const throw();
+		 };
+		 const std::string	getName() const;
+		 int	getGrade() const;
+		 void	setGrade(int grade);
+		 void	incrementGrade();
+		 void	decrementGrade();
+};
+	std::ostream& operator<<(std::ostream& os, const Bureaucrat& b);
